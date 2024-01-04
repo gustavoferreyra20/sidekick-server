862adb7b39b7952fc008bc6b1cd0a4fa81d34528
Gustavo Ferreyra <ferreyragustavo847@gmail.com>
2024-01-04 14:51:26 -0300

    Validate roles in modes

--- src/express/routes/modes.js
+++ src/express/routes/modes.js
@@ -1,4 +1,5 @@
 const { models } = require('../../sequelize/index');
+const isAdmin = require('../utils/isAdmin');
 
 async function getAll(req, res) {
 	const modes = await models.modes.findAll();
@@ -20,34 +21,50 @@ async function getSingle(req, res) {
 async function create(req, res) {
 	const modeData = req.body;
 
-	const mode = await models.modes.create(modeData);
-	res.status(200).json(mode);
+	const adminStatus = await isAdmin(req);
+
+	if (adminStatus) {
+		const mode = await models.modes.create(modeData);
+		res.status(200).json(mode);
+	} else {
+		res.status(401).json({ error: 'Unauthorized' });
+	}
 }
 
 async function update(req, res) {
 	const modeId = req.params.id;
 	const modeData = req.body;
+	const adminStatus = await isAdmin(req);
 
-	const [updatedRows] = await models.modes.update(modeData, {
-		where: { id_mode: modeId },
-	});
+	if (adminStatus) {
+		const mode = await models.modes.findByPk(modeId);
+		if (!mode) {
+			res.status(404).send('404 - Not found');
+		}
 
-	if (updatedRows > 0) {
+		await mode.update(modeData);
+
 		res.status(200).json({ message: 'Updated successfully' });
 	} else {
-		res.status(404).send('404 - Not found');
+		res.status(401).json({ error: 'Unauthorized' });
 	}
 }
 
 async function removeSingle(req, res) {
 	const modeId = req.params.id;
-	const mode = await models.modes.findByPk(modeId);
+	const adminStatus = await isAdmin(req);
 
-	if (mode) {
-		await mode.destroy();
-		res.status(200).json({ message: 'Deleted successfully' });
+	if (adminStatus) {
+		const mode = await models.modes.findByPk(modeId);
+
+		if (mode) {
+			await mode.destroy();
+			res.status(200).json({ message: 'Deleted successfully' });
+		} else {
+			res.status(404).send('404 - Not found');
+		}
 	} else {
-		res.status(404).send('404 - Not found');
+		res.status(401).json({ error: 'Unauthorized' });
 	}
 }
 

a194772058c71172d627d5b27e89b41330719b4d
Gustavo Ferreyra <ferreyragustavo847@gmail.com>
2024-01-05 18:01:48 -0300

    validate roles in rewards

--- src/express/routes/rewards.js
+++ src/express/routes/rewards.js
@@ -1,4 +1,5 @@
 const { models } = require('../../sequelize/index');
+const isAdmin = require('../utils/isAdmin');
 
 models.rewards.belongsToMany(models.users, { through: 'users_rewards', foreignKey: 'id_reward' });
 models.rewards.belongsToMany(models.reviews, { through: 'reviews_rewards', foreignKey: 'id_reward' });
@@ -22,32 +23,52 @@ async function getSingle(req, res) {
 async function create(req, res) {
 	const rewardData = req.body;
 
-	const reward = await models.rewards.create(rewardData);
-	res.status(200).json(reward);
+	const adminStatus = await isAdmin(req);
+
+	if (adminStatus) {
+		const reward = await models.rewards.create(rewardData);
+		res.status(200).json(reward);
+	} else {
+		res.status(401).json({ error: 'Unauthorized' });
+	}
+
 };
 
 async function update(req, res) {
 	const rewardId = req.params.id;
-	const reward = await models.rewards.findByPk(rewardId);
 	const rewardData = req.body;
+	const adminStatus = await isAdmin(req);
 
-	if (reward) {
+	if (adminStatus) {
+		const reward = await models.rewards.findByPk(rewardId);
+
+		if (!reward) {
+			return res.status(404).send('404 - Not found');
+		}
+
 		await reward.update(rewardData);
+
 		res.status(200).json({ message: 'Updated successfully' });
 	} else {
-		res.status(404).send('404 - Not found');
+		res.status(401).json({ error: 'Unauthorized' });
 	}
 };
 
 async function removeSingle(req, res) {
 	const rewardId = req.params.id;
-	const reward = await models.rewards.findByPk(rewardId);
+	const adminStatus = await isAdmin(req);
 
-	if (reward) {
-		await reward.destroy();
-		res.status(200).json({ message: 'Deleted successfully' });
+	if (adminStatus) {
+		const reward = await models.rewards.findByPk(rewardId);
+
+		if (reward) {
+			await reward.destroy();
+			res.status(200).json({ message: 'Deleted successfully' });
+		} else {
+			res.status(404).send('404 - Not found');
+		}
 	} else {
-		res.status(404).send('404 - Not found');
+		res.status(401).json({ error: 'Unauthorized' });
 	}
 };
 

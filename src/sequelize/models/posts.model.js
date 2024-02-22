7af4b2fcb456d96b8ce70762598f48a21373eb39
Gustavo Ferreyra <ferreyragustavo847@gmail.com>
2024-02-22 09:13:11 -0300

    Update posts.model.js

--- src/sequelize/models/posts.model.js
+++ src/sequelize/models/posts.model.js
@@ -11,18 +11,27 @@ module.exports = (sequelize) => {
         id_game: DataTypes.INTEGER,
         id_platform: DataTypes.INTEGER,
         id_mode: DataTypes.INTEGER,
-        requiredusers : DataTypes.INTEGER,
+        requiredusers: DataTypes.INTEGER,
         actualusers: DataTypes.INTEGER,
         title: DataTypes.STRING,
         description: DataTypes.STRING,
         date: DataTypes.TIME,
-        deleted: { type: DataTypes.BOOLEAN, defaultValue: false }
+        deleted: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false,
+            allowNull: false,
+            set: function (value) {
+                if (value === 'true') value = true;
+                if (value === 'false') value = false;
+                this.setDataValue('deleted', value);
+            }
+        }
     }, {
         freezeTableName: true,
         timestamps: false,
         defaultScope: {
             where: {
-                deleted: 0
+                deleted: false
             }
         }
     });

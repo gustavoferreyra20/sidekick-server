40768a9838ce829c1b064fa96dc49b4e0200da5c
Gustavo Ferreyra <ferreyragustavo847@gmail.com>
2024-02-22 01:19:23 -0300

    Update notifications.model.js

--- src/sequelize/models/notifications.model.js
+++ src/sequelize/models/notifications.model.js
@@ -21,7 +21,7 @@ module.exports = (sequelize) => {
         timestamps: false,
         defaultScope: {
             where: {
-                deleted: 0
+                deleted: false
             }
         }
     });
